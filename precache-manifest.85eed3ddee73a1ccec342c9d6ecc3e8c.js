a8509f662906b3bb566dc8f4b3b75a317752bc21
Haolin Yang <haolinyang95@gmail.com>
2020-03-26 23:26:49 -0400

    Updates

---
+++ precache-manifest.85eed3ddee73a1ccec342c9d6ecc3e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4b7c854e63b629eede803077e8ebc2",
+    "url": "//top-20-viewed-news/index.html"
+  },
+  {
+    "revision": "26a3714efa2183f35ba1",
+    "url": "//top-20-viewed-news/static/css/main.3e447901.chunk.css"
+  },
+  {
+    "revision": "c6ff501b840f83e29ed5",
+    "url": "//top-20-viewed-news/static/js/2.eec75ced.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "//top-20-viewed-news/static/js/2.eec75ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a3714efa2183f35ba1",
+    "url": "//top-20-viewed-news/static/js/main.85ec4ad8.chunk.js"
+  },
+  {
+    "revision": "1ef0664fb3619ab53e46",
+    "url": "//top-20-viewed-news/static/js/runtime-main.b6cd1817.js"
+  }
+]);
